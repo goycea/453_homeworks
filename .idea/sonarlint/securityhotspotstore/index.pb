378307cf45a8dbf9150cdafc0744ff04c7ee9d16
Mehlika <140068255+MehlikaErgl@users.noreply.github.com>
2024-11-09 23:03:52 +0300

    hw2-part2

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+]
+-hw2/src/test/java/org/example/MarketTest.java,4\6\469ddf8520dea7a7d6ab204b63eb2d70ba6c6715
+_
+/hw2/src/main/java/org/example/ProductModel.java,d\8\d8f5cb0586b14b7245e1f148b4281ee6022bdf37
+W
+'hw2/src/main/java/org/example/Main.java,f\f\ff93024797377b3dca24f9f9bff71d0dbf4964d2
---
+++ hw2/src/test/java/org/example/BasisPathTests.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasisPathTests {
+
+    private ShoppingCart cart;
+    private ProductModel product1;
+
+    @BeforeEach
+    void setUp() {
+        cart = new ShoppingCart();
+        product1 = new ProductModel("Apple", 1, 1, "kg");
+    }
+
+    //Path 1: There is no product, the process is terminated.
+    @Test
+    void testIsProductAvailable() {
+        int removedQuantity = cart.removeProduct(new ProductModel("Orange", 1, 1, "kg"), 1);
+        assertEquals(0, removedQuantity);
+    }
+
+    //Path 2: Product exists, quantity is insufficient, the process is terminated.
+    @Test
+    void testIsQuantityEnough() {
+        cart.addProduct(product1, 1);
+        int removedQuantity = cart.removeProduct(product1, 2);
+        assertEquals(0, removedQuantity);
+        assertEquals(1, cart.getProducts()[0].getQuantity());
+    }
+
+    //Path 3: The product exists, the quantity is sufficient, the product quantity decreases, it does not become zero, the total cost is updated.
+    @Test
+    void testIfQuantityEnough() {
+        cart.addProduct(product1, 3);
+        int removedQuantity = cart.removeProduct(product1, 1);
+        assertEquals(1, removedQuantity);
+        assertEquals(2, cart.getProducts()[0].getQuantity());
+        assertEquals(2, cart.getTotalCost());
+    }
+
+    //Path 4: The product exists, the quantity is sufficient, the product quantity decreases to zero, the product is removed from the list and the total cost is updated.
+    @Test
+    void testRemovingFromList() {
+        cart.addProduct(product1, 1);
+        int removedQuantity = cart.removeProduct(product1, 1);
+        assertEquals(1, removedQuantity);
+        assertEquals(0, cart.getTotalProducts());
+        assertEquals(0, cart.getTotalCost());
+    }
+
+}
