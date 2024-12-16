b209b7e472c0593bf0e437eb5159ffb059393e75
Alperen Göyce <100462166+goycea@users.noreply.github.com>
2024-12-16 16:38:00 +0300

    change driver string

--- hw4/src/main/java/org/example/Main.java
+++ hw4/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:/Users/alper/OneDrive/Masaüstü/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
         WebDriver driver = new ChromeDriver();
 
         // PTT AVM
